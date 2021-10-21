03eac791ff84e5fce1b7b099b58db9c871ca3b14
jaym21 <jaynmistry21@gmail.com>
2021-10-21 23:13:57 +0530

    added viewHolder and diffUtilCallback

--- app/src/main/java/dev/jaym21/cryptowatch/adapters/CurrencyRVAdapter.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/adapters/CurrencyRVAdapter.kt
@@ -1,14 +1,36 @@
 package dev.jaym21.cryptowatch.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import dev.jaym21.cryptoapi.models.entities.Currency
 import dev.jaym21.cryptowatch.databinding.RvCurrencyItemBinding
 
-class CurrencyRVAdapter: ListAdapter<Currency, CurrencyRVAdapter.CurrencyViewHolder>() {
+class CurrencyRVAdapter: ListAdapter<Currency, CurrencyRVAdapter.CurrencyViewHolder>(CurrencyDiffCallback()) {
 
+    class CurrencyDiffCallback: DiffUtil.ItemCallback<Currency>() {
+        override fun areItemsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return oldItem == newItem
+        }
 
+        override fun areContentsTheSame(oldItem: Currency, newItem: Currency): Boolean {
+            return oldItem.toString() == newItem.toString()
+        }
+    }
 
     inner class CurrencyViewHolder(val binding: RvCurrencyItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrencyViewHolder {
+        val inflater = parent.context.getSystemService(LayoutInflater::class.java)
+        val binding = RvCurrencyItemBinding.inflate(inflater)
+        return CurrencyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.binding.tvCurrencyName.text = currentItem.name
+    }
 }
