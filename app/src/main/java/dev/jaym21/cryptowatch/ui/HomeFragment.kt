ec39a363d8ce87edd1f7dc841067b836ff3bc351
jaym21 <jaynmistry21@gmail.com>
2021-10-22 12:43:59 +0530

    added internet permission

--- app/src/main/java/dev/jaym21/cryptowatch/ui/HomeFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/HomeFragment.kt
@@ -1,19 +1,26 @@
 package dev.jaym21.cryptowatch.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
 import dev.jaym21.cryptowatch.R
 import dev.jaym21.cryptowatch.adapters.CurrencyRVAdapter
 import dev.jaym21.cryptowatch.databinding.FragmentHomeBinding
+import dev.jaym21.cryptowatch.utils.ApiResponse
 
 class HomeFragment : Fragment() {
 
     private var binding: FragmentHomeBinding? = null
     private val currencyAdapter = CurrencyRVAdapter()
+    private lateinit var viewModel: HomeViewModel
+    private val TAG ="HomeFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +36,30 @@ class HomeFragment : Fragment() {
 
         //initializing recyclerView
         setUpRecyclerView()
+
+        //initializing viewModel
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        //calling function to get currencies
+        viewModel.getCurrencies()
+
+
+        //observing the currencies LiveData to get currencies data for recycler view
+        viewModel.currencies.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is ApiResponse.Success -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    currencyAdapter.submitList(response.data)
+                }
+                is ApiResponse.Loading -> {
+                    binding?.progressBar?.visibility = View.VISIBLE
+                }
+                is ApiResponse.Error -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    Snackbar.make(view, "Could retrieve currencies, restart app!", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 
     private fun setUpRecyclerView() {
--- app/src/main/java/dev/jaym21/cryptowatch/ui/HomeViewModel.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/HomeViewModel.kt
@@ -1,7 +1,37 @@
 package dev.jaym21.cryptowatch.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.jaym21.cryptoapi.models.responses.Currency
+import dev.jaym21.cryptowatch.data.NomicsRepository
+import dev.jaym21.cryptowatch.utils.ApiResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeViewModel: ViewModel() {
 
+    private val repo = NomicsRepository()
+
+    //live data for currency response
+    val currencies: MutableLiveData<ApiResponse<List<Currency>>> = MutableLiveData()
+
+    fun getCurrencies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            //as we are going to make network call so showing loading progress bar
+            currencies.postValue(ApiResponse.Loading())
+
+            //getting response from repo
+            val response = repo.getCurrencies()
+            //checking if we got a successful response
+            if (response!!.isNotEmpty()){
+                response.let {
+                    currencies.postValue(ApiResponse.Success(it))
+                }
+            }else {
+                currencies.postValue(ApiResponse.Error("Could not retrieve currencies, try again!"))
+            }
+        }
+    }
 }
