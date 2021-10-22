c119abfbf87af7ad606ac033d2451d83bc360d7f
jaym21 <jaynmistry21@gmail.com>
2021-10-22 10:00:28 +0530

    implemented setting up recycler view

--- app/src/main/java/dev/jaym21/cryptowatch/ui/HomeFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/HomeFragment.kt
@@ -5,15 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.adapters.CurrencyRVAdapter
+import dev.jaym21.cryptowatch.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
+    private val currencyAdapter = CurrencyRVAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initializing recyclerView
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        binding?.rvCurrencies?.apply {
+            adapter = currencyAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
