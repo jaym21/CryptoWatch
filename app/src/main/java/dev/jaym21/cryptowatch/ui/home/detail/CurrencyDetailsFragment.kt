badb0277687b121dd4ee6028e8380313b143ca5a
jaym21 <jaynmistry21@gmail.com>
2021-11-04 18:12:41 +0530

    implementing displaying current price and currency name

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/CurrencyDetailsFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/CurrencyDetailsFragment.kt
@@ -1,16 +1,33 @@
 package dev.jaym21.cryptowatch.ui.home.detail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.highlight.Highlight
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener
+import com.google.android.material.snackbar.Snackbar
+import dev.jaym21.cryptowatch.R
 import dev.jaym21.cryptowatch.databinding.FragmentCurrencyDetailsBinding
-class CurrencyDetailsFragment : Fragment() {
+import dev.jaym21.cryptowatch.utils.ApiResponse
+import dev.jaym21.cryptowatch.utils.SVGLoader
+
+class CurrencyDetailsFragment : Fragment(), OnChartValueSelectedListener {
 
     private var binding: FragmentCurrencyDetailsBinding? = null
+    private var TAG = "CurrencyDetailsFragment"
     private lateinit var viewModel: CurrencyDetailsViewModel
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +41,91 @@ class CurrencyDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //initializing navController
+        navController = Navigation.findNavController(view)
+
+        binding?.ivBackButton?.setOnClickListener {
+            navController.popBackStack()
+        }
+
         viewModel = ViewModelProvider(this).get(CurrencyDetailsViewModel::class.java)
+
+        viewModel.getCurrencyDetails("BTC")
+
+        viewModel.currencyDetails.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is ApiResponse.Success -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    binding?.tvCurrencyName?.text = response.data!![0].name
+                    binding?.tvCurrentPrice?.text = response.data[0].price
+
+                    val length = response.data[0].logoUrl!!.length
+                    if (response.data[0].logoUrl!!.substring(length - 3) == "svg"){
+                        SVGLoader.fetchSvg(binding!!.root.context, response.data[0].logoUrl!!, binding!!.ivCurrencyIcon)
+                    }else {
+                        Glide.with(binding!!.root.context).load(response.data[0].logoUrl).transform(
+                            RoundedCorners(100)
+                        ) .into(binding!!.ivCurrencyIcon)
+                    }
+
+                    if (response.data[0].oneDay != null) {
+
+                        if (response.data[0].oneDay!!.priceChangePct!!.toDouble() >= 0) {
+                            binding!!.tvPercentChange.text = response.data[0].oneDay!!.priceChangePct!! + " %"
+
+                            Glide.with(binding!!.root.context).load(R.drawable.ic_trending_up).into(binding!!.ivChangeIcon)
+                            binding!!.ivChangeIcon.setColorFilter(ContextCompat.getColor(binding!!.root.context, R.color.green))
+                            binding!!.tvPercentChange.setTextColor(
+                                ContextCompat.getColor(
+                                    binding!!.root.context,
+                                    R.color.green
+                                )
+                            )
+                        } else {
+                            binding!!.tvPercentChange.text = response.data[0].oneDay!!.priceChangePct!!.substring(1) + " %"
+
+                            Glide.with(binding!!.root.context).load(R.drawable.ic_trending_down).into(binding!!.ivChangeIcon)
+                            binding!!.ivChangeIcon.setColorFilter(ContextCompat.getColor(binding!!.root.context, R.color.red))
+                            binding!!.tvPercentChange.setTextColor(
+                                ContextCompat.getColor(
+                                    binding!!.root.context,
+                                    R.color.red
+                                )
+                            )
+                        }
+
+                    }
+
+                    //line chart
+                    // disable description text
+                    binding?.chart?.description?.isEnabled = false
+
+                    // enable touch gestures
+                    binding?.chart?.setTouchEnabled(true)
+
+                    binding?.chart?.setOnChartValueSelectedListener(this);
+                    binding?.chart?.setDrawGridBackground(false);
+                }
+
+                is ApiResponse.Loading -> {
+                    binding?.progressBar?.visibility = View.VISIBLE
+                }
+
+                is ApiResponse.Error -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    Snackbar.make(view, "Could retrieve details, restart app!", Snackbar.LENGTH_SHORT).show()
+                    navController.popBackStack()
+                }
+            }
+        })
+    }
+
+    override fun onValueSelected(e: Entry?, h: Highlight?) {
+        Log.d(TAG, "onValueSelected: ENTRY SELECTED ${e.toString()}")
+    }
+
+    override fun onNothingSelected() {
+        Log.d(TAG, "NOTHING SELECTED")
     }
 
     override fun onDestroy() {
--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/CurrencyDetailsViewModel.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/CurrencyDetailsViewModel.kt
@@ -3,7 +3,9 @@ package dev.jaym21.cryptowatch.ui.home.detail
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dev.jaym21.cryptoapi.models.entities.Data
 import dev.jaym21.cryptoapi.models.responses.CurrencyResponse
+import dev.jaym21.cryptowatch.data.CryptoCompareRepository
 import dev.jaym21.cryptowatch.data.NomicsRepository
 import dev.jaym21.cryptowatch.utils.ApiResponse
 import kotlinx.coroutines.Dispatchers
@@ -11,17 +13,20 @@ import kotlinx.coroutines.launch
 
 class CurrencyDetailsViewModel: ViewModel() {
 
-    private val repo = NomicsRepository()
+    private val nomicsRepository = NomicsRepository()
+    private val cryptoCompareRepository = CryptoCompareRepository()
 
     //live data for currency details response
     val currencyDetails: MutableLiveData<ApiResponse<List<CurrencyResponse>>> = MutableLiveData()
+    //live data for currency history response
+    val currencyHistory: MutableLiveData<ApiResponse<Data>> = MutableLiveData()
 
     fun getCurrencyDetails(id: String) = viewModelScope.launch(Dispatchers.IO) {
         //as we are going to make network call so showing loading progress bar
         currencyDetails.postValue(ApiResponse.Loading())
 
         //getting response from repo
-        val response = repo.getCurrencyDetails(id)
+        val response = nomicsRepository.getCurrencyDetails(id)
         //checking if we got a successful response
         if (response!!.isNotEmpty()) {
             response.let {
@@ -29,6 +34,21 @@ class CurrencyDetailsViewModel: ViewModel() {
             }
         } else {
             currencyDetails.postValue(ApiResponse.Error("Could not retrieve details, try again!"))
+        }
+    }
+
+    fun getCurrencyHistory(requiredCurrency: String, convertTo: String, requiredTime: String) = viewModelScope.launch(Dispatchers.IO) {
+        //as we are going to make network call so showing loading progress bar
+        currencyHistory.postValue(ApiResponse.Loading())
+
+        val response = cryptoCompareRepository.getHistoricalData(requiredCurrency, convertTo, requiredTime)
+        //checking if we got a successful response
+        if (response?.data!!.isNotEmpty()) {
+            response.let {
+                currencyHistory.postValue(ApiResponse.Success(it))
+            }
+        } else {
+            currencyHistory.postValue(ApiResponse.Error("Could retrieve historical data, try again!"))
         }
     }
 }
