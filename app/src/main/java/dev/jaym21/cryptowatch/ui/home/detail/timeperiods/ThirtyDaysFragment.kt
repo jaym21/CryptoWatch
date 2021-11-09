c99957b49e948aa98f54b996b78faa106119f7e5
jaym21 <jaynmistry21@gmail.com>
2021-11-09 17:42:45 +0530

    implementing thirty days chart

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/ThirtyDaysFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/ThirtyDaysFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.data.Entry
 import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.databinding.FragmentThirtyDaysBinding
+import dev.jaym21.cryptowatch.ui.home.detail.CurrencyDetailsViewModel
+import dev.jaym21.cryptowatch.utils.CustomMarkerView
 
-class ThirtyDaysFragment : Fragment() {
+class ThirtyDaysFragment(val currencyId: String, val convertTo: String, val isChangePositive: Boolean) : Fragment() {
+
+    private var binding: FragmentThirtyDaysBinding? = null
+    private lateinit var viewModel: CurrencyDetailsViewModel
+    private lateinit var customMarkerView: CustomMarkerView
+    private var entriesThirtyDays = arrayListOf<Entry>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thirty_days, container, false)
+        binding = FragmentThirtyDaysBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
