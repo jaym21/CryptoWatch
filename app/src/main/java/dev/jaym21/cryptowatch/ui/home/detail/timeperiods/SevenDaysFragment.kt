6f49eb181c178fb490f3f587f3965dbe2579833f
jaym21 <jaynmistry21@gmail.com>
2021-11-09 17:39:16 +0530

    implemented seven days chart fragment

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/SevenDaysFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/SevenDaysFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.github.mikephil.charting.animation.Easing
 import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.google.android.material.snackbar.Snackbar
 import dev.jaym21.cryptowatch.R
 import dev.jaym21.cryptowatch.databinding.FragmentSevenDaysBinding
 import dev.jaym21.cryptowatch.ui.home.detail.CurrencyDetailsViewModel
+import dev.jaym21.cryptowatch.utils.ApiResponse
 import dev.jaym21.cryptowatch.utils.CustomMarkerView
 
 class SevenDaysFragment(val currencyId: String, val convertTo: String, val isChangePositive: Boolean) : Fragment() {
@@ -39,6 +46,72 @@ class SevenDaysFragment(val currencyId: String, val convertTo: String, val isCha
 
         //calling for hourly data of currency
         viewModel.getCurrencyDailyHistory(currencyId ,convertTo, "7")
+
+        //styling chart
+        binding?.chart?.axisLeft?.isEnabled = false
+        binding?.chart?.axisRight?.isEnabled = false
+        binding?.chart?.xAxis?.isEnabled = false
+        binding?.chart?.legend?.isEnabled = false
+        binding?.chart?.description?.isEnabled = false
+        binding?.chart?.setTouchEnabled(true)
+        binding?.chart?.isDragEnabled = true
+        binding?.chart?.setScaleEnabled(false)
+        binding?.chart?.setPinchZoom(false)
+        binding?.chart?.marker = customMarkerView
+
+        viewModel.currencyDailyHistory.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is ApiResponse.Success -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    entriesSevenDays.clear()
+                    response.data?.data?.forEach{
+                        entriesSevenDays.add(Entry(it.time!!.toFloat(), it.close!!.toFloat()))
+                    }
+                    val dataSet = LineDataSet(entriesSevenDays, "line chart")
+                    updateChart(dataSet)
+                }
+                is ApiResponse.Loading -> {
+                    binding?.progressBar?.visibility = View.VISIBLE
+                }
+                is ApiResponse.Error -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    Snackbar.make(view, "Could retrieve details, restart app!", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        })
+    }
+
+    private fun updateChart(lineDataSet: LineDataSet) {
+        lineDataSet.lineWidth = 2f
+        lineDataSet.setDrawFilled(true)
+        lineDataSet.setDrawHighlightIndicators(false)
+        lineDataSet.setDrawCircleHole(false)
+        lineDataSet.setDrawCircles(false)
+        lineDataSet.setDrawValues(false)
+        lineDataSet.setDrawIcons(false)
+        lineDataSet.disableDashedLine()
+
+        if (isChangePositive) {
+            lineDataSet.fillDrawable = ContextCompat.getDrawable(binding!!.root.context, R.drawable.chart_fade_green)
+            lineDataSet.color = ContextCompat.getColor(
+                binding!!.root.context,
+                R.color.green
+            )
+        } else {
+            lineDataSet.fillDrawable = ContextCompat.getDrawable(binding!!.root.context, R.drawable.chart_fade_red)
+            lineDataSet.color = ContextCompat.getColor(
+                binding!!.root.context,
+                R.color.red
+            )
+        }
+
+        val data = LineData(lineDataSet)
+
+        data.setDrawValues(false)
+
+        binding?.chart?.data = data
+        binding?.chart?.animateXY(3000, 3000, Easing.EaseInCubic)
+        binding?.chart?.invalidate()
     }
 
     override fun onDestroy() {
