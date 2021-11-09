5e69dd3f7dc76aa00bc647cbb55fe737876ffebb
jaym21 <jaynmistry21@gmail.com>
2021-11-09 17:53:52 +0530

    implementing one year chart fragment

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/OneYearFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/detail/timeperiods/OneYearFragment.kt
@@ -5,15 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.data.Entry
 import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.databinding.FragmentOneYearBinding
+import dev.jaym21.cryptowatch.ui.home.detail.CurrencyDetailsViewModel
+import dev.jaym21.cryptowatch.utils.CustomMarkerView
 
-class OneYearFragment : Fragment() {
+class OneYearFragment(val currencyId: String, val convertTo: String, val isChangePositive: Boolean) : Fragment() {
+
+    private var binding: FragmentOneYearBinding? = null
+    private lateinit var viewModel: CurrencyDetailsViewModel
+    private lateinit var customMarkerView: CustomMarkerView
+    private var entriesSevenDays = arrayListOf<Entry>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_one_year, container, false)
+        binding = FragmentOneYearBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
