73ce38cb248f2fd3878dce993bce6240eb1ba4d3
jaym21 <jaynmistry21@gmail.com>
2021-11-12 10:00:55 +0530

    updated splash screen

--- app/src/main/java/dev/jaym21/cryptowatch/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_CryptoWatch)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
 
