912725e29a79e04939f925f2c2ecede99a98bcf0
jaym21 <jaynmistry21@gmail.com>
2021-11-12 22:32:37 +0530

    changing layout viewPager and tablayout

--- app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistFragment.kt
@@ -5,13 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.jaym21.cryptowatch.R
+import com.google.android.material.tabs.TabLayoutMediator
+import dev.jaym21.cryptowatch.adapters.WatchlistViewPagerAdapter
 import dev.jaym21.cryptowatch.databinding.FragmentWatchlistBinding
 
 class WatchlistFragment: Fragment() {
 
     private var binding: FragmentWatchlistBinding? = null
-
+    private lateinit var watchlistViewPagerAdapter: WatchlistViewPagerAdapter
+    var watchlists = arrayOf("Watchlist 1", "Watchlist 2", "Watchlist 3", "Watchlist 4")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,6 +21,19 @@ class WatchlistFragment: Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentWatchlistBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initializing viewPager adapter
+        watchlistViewPagerAdapter = WatchlistViewPagerAdapter(parentFragmentManager, lifecycle)
+        //setting adapter to viewPager
+        binding?.vpWatchlist?.adapter = watchlistViewPagerAdapter
+        //integrating tablayout
+        TabLayoutMediator(binding?.tlWatchlist!!, binding?.vpWatchlist!!) { tab, position ->
+            tab.text = watchlists[position]
+        }
     }
 
     override fun onDestroy() {
