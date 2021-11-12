e59b220ee79e1831d09feddccfca0d94a4661020
jaym21 <jaynmistry21@gmail.com>
2021-11-12 22:18:21 +0530

    implemented watchlist viewPager adapter

--- app/src/main/java/dev/jaym21/cryptowatch/adapters/WatchlistViewPagerAdapter.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/adapters/WatchlistViewPagerAdapter.kt
@@ -4,6 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import dev.jaym21.cryptowatch.ui.watchlist.WatchlistFourFragment
+import dev.jaym21.cryptowatch.ui.watchlist.WatchlistOneFragment
+import dev.jaym21.cryptowatch.ui.watchlist.WatchlistThreeFragment
+import dev.jaym21.cryptowatch.ui.watchlist.WatchlistTwoFragment
 
 private const val WATCHLIST_TABS = 4
 
@@ -14,7 +18,10 @@ class WatchlistViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lif
 
     override fun createFragment(position: Int): Fragment {
         when(position) {
-            0 ->
+            0 -> return WatchlistOneFragment()
+            1 -> return WatchlistTwoFragment()
+            2 -> return WatchlistThreeFragment()
         }
+        return WatchlistFourFragment()
     }
 }
