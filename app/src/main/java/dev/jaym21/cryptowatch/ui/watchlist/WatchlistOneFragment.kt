21d7e992c236f28002f48464475261d37892f845
jaym21 <jaynmistry21@gmail.com>
2021-11-13 12:24:05 +0530

    implemented fetching watchlist one currencies from database

--- app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistOneFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistOneFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import dev.jaym21.cryptowatch.R
 import dev.jaym21.cryptowatch.databinding.FragmentWatchlistOneBinding
+import dev.jaym21.cryptowatch.utils.ApiResponse
 
 class WatchlistOneFragment : Fragment() {
 
@@ -29,7 +31,26 @@ class WatchlistOneFragment : Fragment() {
         //initializing watchlist viewModel
         watchlistViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)).get(WatchlistViewModel::class.java)
 
-        //initializing
+        watchlistViewModel.allCurrenciesInWatchlist.observe(viewLifecycleOwner, Observer { allWatchlists ->
+            var requiredCurrencies = ""
+            allWatchlists.forEach {
+                if (it.watchlistNumber == "1") {
+                    if (requiredCurrencies.isEmpty()) {
+                        requiredCurrencies = it.symbol
+                    } else {
+                        requiredCurrencies += ", ${it.symbol}"
+                    }
+                }
+            }
+            if (requiredCurrencies.isNotEmpty())
+                watchlistViewModel.fetchCurrencies(requiredCurrencies)
+        })
+
+        watchlistViewModel.requiredCurrencies.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is ApiResponse.Success ->
+            }
+        })
     }
 
     override fun onDestroy() {
