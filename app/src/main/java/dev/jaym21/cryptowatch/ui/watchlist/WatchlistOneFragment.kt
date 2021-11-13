cbb2faeb752d82b7a4d7749db866164f2800dbf9
jaym21 <jaynmistry21@gmail.com>
2021-11-13 10:10:45 +0530

    removed watchlist adapter

--- app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistOneFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistOneFragment.kt
@@ -5,15 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.databinding.FragmentWatchlistOneBinding
 
 class WatchlistOneFragment : Fragment() {
+
+    private var binding: FragmentWatchlistOneBinding? = null
+    lateinit var viewModel: WatchlistViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_watchlist_one, container, false)
+        binding = FragmentWatchlistOneBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initializing viewModel
+        viewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)).get(WatchlistViewModel::class.java)
+
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
