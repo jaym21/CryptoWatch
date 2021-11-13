5c9845f8d162101b2851dc126ec7cbe8d9316881
jaym21 <jaynmistry21@gmail.com>
2021-11-13 09:38:26 +0530

    added insert, delete and get all functions inside DAO

--- app/src/main/java/dev/jaym21/cryptowatch/database/WatchlistDAO.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/database/WatchlistDAO.kt
@@ -1,6 +1,18 @@
 package dev.jaym21.cryptowatch.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import dev.jaym21.cryptowatch.model.Watchlist
+
+@Dao
 interface WatchlistDAO {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertCurrency(watchlist: Watchlist)
 
+    @Delete
+    suspend fun deleteCurrency(watchlist: Watchlist)
+
+    @Query("SELECT * FROM watchlist_table")
+    fun getAllCurrencies(): LiveData<List<Watchlist>>
 }
