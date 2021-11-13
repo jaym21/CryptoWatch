b4ce9bc9ef0dbbec52562f1f15fb753a5204e9ef
jaym21 <jaynmistry21@gmail.com>
2021-11-13 09:53:19 +0530

    implementing viewModel for watchlists

---
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistViewModel.kt
@@ -0,0 +1,17 @@
+package dev.jaym21.cryptowatch.ui.watchlist
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import dev.jaym21.cryptowatch.data.WatchlistRepository
+import dev.jaym21.cryptowatch.model.Watchlist
+
+class WatchlistViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repo: WatchlistRepository
+    val allCurrenciesInWatchlist: LiveData<List<Watchlist>>
+
+    init {
+
+    }
+}
