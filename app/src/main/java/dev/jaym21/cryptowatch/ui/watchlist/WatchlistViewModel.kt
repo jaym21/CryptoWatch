31a4e7e77e650785e4fb69fd7ff0246dc85ae858
jaym21 <jaynmistry21@gmail.com>
2021-11-13 09:58:53 +0530

    implemented functions in watchlist viewModel

--- app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistViewModel.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/watchlist/WatchlistViewModel.kt
@@ -3,9 +3,12 @@ package dev.jaym21.cryptowatch.ui.watchlist
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import dev.jaym21.cryptowatch.data.WatchlistRepository
 import dev.jaym21.cryptowatch.database.WatchlistDatabase
 import dev.jaym21.cryptowatch.model.Watchlist
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class WatchlistViewModel(application: Application): AndroidViewModel(application) {
 
@@ -15,6 +18,14 @@ class WatchlistViewModel(application: Application): AndroidViewModel(application
     init {
         val dao = WatchlistDatabase.getDatabase(application).getWatchlistDAO()
         repo = WatchlistRepository(dao)
-        allCurrenciesInWatchlist = repo.getAllCurrencies()
+        allCurrenciesInWatchlist = repo.getAllCurrencies
+    }
+
+    fun addCurrencyToWatchlist(watchlist: Watchlist) = viewModelScope.launch(Dispatchers.IO) {
+        repo.addCurrency(watchlist)
+    }
+
+    fun removeCurrencyFromWatchlist(watchlist: Watchlist) = viewModelScope.launch(Dispatchers.IO) {
+        repo.removeCurrency(watchlist)
     }
 }
