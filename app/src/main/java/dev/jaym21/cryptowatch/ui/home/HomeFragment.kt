3d0bda542b90e18d7f26f55e15051b929ce56b45
jaym21 <jaynmistry21@gmail.com>
2021-12-09 12:36:34 +0530

    adding text watcher to search edittext

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package dev.jaym21.cryptowatch.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -63,6 +65,21 @@ class HomeFragment : Fragment(), ICurrencyRVAdapter {
         //calling function to get currencies
         currentPage = 1
         viewModel.getCurrencies(currentPage.toString(), true)
+
+        //adding text watcher on search edit text
+        binding?.etSearch?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(editable: Editable?) {
+
+            }
+        })
 
         //observing the currencies LiveData to get currencies data for recycler view
         viewModel.currencies.observe(viewLifecycleOwner, Observer { response ->
