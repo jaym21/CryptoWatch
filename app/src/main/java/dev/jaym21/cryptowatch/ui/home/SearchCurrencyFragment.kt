586aa5290ac7d3305cf427f011c825d1c247a506
jaym21 <jaynmistry21@gmail.com>
2021-12-10 12:24:10 +0530

    added progress bar to search fragment

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/SearchCurrencyFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/SearchCurrencyFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import dev.jaym21.cryptoapi.models.responses.CurrencyResponse
 import dev.jaym21.cryptowatch.R
 import dev.jaym21.cryptowatch.adapters.CurrencyRVAdapter
 import dev.jaym21.cryptowatch.adapters.ICurrencyRVAdapter
 import dev.jaym21.cryptowatch.databinding.FragmentSearchCurrencyBinding
+import dev.jaym21.cryptowatch.utils.ApiResponse
 
 class SearchCurrencyFragment : Fragment(), ICurrencyRVAdapter {
 
@@ -20,6 +24,8 @@ class SearchCurrencyFragment : Fragment(), ICurrencyRVAdapter {
     private lateinit var navController: NavController
     private lateinit var currencyAdapter: CurrencyRVAdapter
     private lateinit var viewModel: HomeViewModel
+    private var searchedText: String? = null
+    private var searchedCurrencies = arrayListOf<CurrencyResponse>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,6 +39,9 @@ class SearchCurrencyFragment : Fragment(), ICurrencyRVAdapter {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //getting searched text from home fragment
+        searchedText = arguments?.getString("searchedText")
+
         //initializing navController
         navController = Navigation.findNavController(view)
 
@@ -44,6 +53,35 @@ class SearchCurrencyFragment : Fragment(), ICurrencyRVAdapter {
 
         //initializing recyclerView
         setUpRecyclerView()
+
+        //making request to get all currencies
+        viewModel.getAllCurrencies()
+
+        viewModel.allCurrencies.observe(viewLifecycleOwner, Observer { response ->
+            when(response) {
+                is ApiResponse.Success -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    searchedCurrencies.clear()
+                    response.data?.forEach {
+                        if (it.name?.contains(searchedText, true) == true) {
+                            searchedCurrencies.add(it)
+                        }
+                    }
+                    currencyAdapter.submitList(searchedCurrencies)
+                }
+                is ApiResponse.Loading -> {
+                    binding?.progressBar?.visibility = View.VISIBLE
+                }
+                is ApiResponse.Error -> {
+                    binding?.progressBar?.visibility = View.GONE
+                    Snackbar.make(
+                        binding?.root!!,
+                        "Could retrieve currencies, restart app!",
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
     }
 
     private fun setUpRecyclerView() {
