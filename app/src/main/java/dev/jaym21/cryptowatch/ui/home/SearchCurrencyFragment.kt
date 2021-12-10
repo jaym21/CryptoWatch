556db7a57856949698c665fc9680ad3dcdce64c3
jaym21 <jaynmistry21@gmail.com>
2021-12-10 12:17:43 +0530

    added viewModel to search fragment

--- app/src/main/java/dev/jaym21/cryptowatch/ui/home/SearchCurrencyFragment.kt
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/SearchCurrencyFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.adapters.CurrencyRVAdapter
 import dev.jaym21.cryptowatch.databinding.FragmentSearchCurrencyBinding
 
 class SearchCurrencyFragment : Fragment() {
 
     private var binding: FragmentSearchCurrencyBinding? = null
+    private lateinit var currencyAdapter: CurrencyRVAdapter
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,19 @@ class SearchCurrencyFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentSearchCurrencyBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //initializing viewModel
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+    }
+
+    private fun setUpRecyclerView() {
+        binding?.rvSearchedCurrencies?.apply {
+            adapter =
+        }
     }
 
     override fun onDestroy() {
