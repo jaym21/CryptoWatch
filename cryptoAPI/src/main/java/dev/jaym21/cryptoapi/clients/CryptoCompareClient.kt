6f8f771875faef1002c88a86a7166a2ce6dd5afb
jaym21 <jaynmistry21@gmail.com>
2021-12-10 14:12:47 +0530

    added okhttpClient to increase connect and read timeout time

--- cryptoAPI/src/main/java/dev/jaym21/cryptoapi/clients/CryptoCompareClient.kt
+++ cryptoAPI/src/main/java/dev/jaym21/cryptoapi/clients/CryptoCompareClient.kt
@@ -1,16 +1,26 @@
 package dev.jaym21.cryptoapi.clients
 
 import dev.jaym21.cryptoapi.services.CryptoCompareAPI
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object CryptoCompareClient {
     private const val BASE_URL = "https://min-api.cryptocompare.com/data/v2/"
+
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
 
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
--- cryptoAPI/src/main/java/dev/jaym21/cryptoapi/clients/NomicsClient.kt
+++ cryptoAPI/src/main/java/dev/jaym21/cryptoapi/clients/NomicsClient.kt
@@ -1,18 +1,28 @@
 package dev.jaym21.cryptoapi.clients
 
 import dev.jaym21.cryptoapi.services.NomicsAPI
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 const val KEY = "71ddbd0f0c76c5000cc68ce4ac71e07f28115729"
 
 object NomicsClient {
     private const val BASE_URL = "https://api.nomics.com/v1/"
 
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
