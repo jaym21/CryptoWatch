a15672d719b0b0c383274f3c3ed71f859fe9cdc3
jaym21 <jaynmistry21@gmail.com>
2021-12-10 12:13:02 +0530

    added search currency fragment

---
+++ app/src/main/java/dev/jaym21/cryptowatch/ui/home/SearchCurrencyFragment.kt
@@ -0,0 +1,28 @@
+package dev.jaym21.cryptowatch.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.jaym21.cryptowatch.R
+import dev.jaym21.cryptowatch.databinding.FragmentSearchCurrencyBinding
+
+class SearchCurrencyFragment : Fragment() {
+
+    private var binding: FragmentSearchCurrencyBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentSearchCurrencyBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
